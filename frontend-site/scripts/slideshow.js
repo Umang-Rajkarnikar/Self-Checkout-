da568ae7b2582ec4aeae4e99451740734d777488
Jonathan Chen <jc09310@gmail.com>
2021-09-18 16:37:23 -0400

    Slideshow script

---
+++ frontend-site/scripts/slideshow.js
@@ -0,0 +1,29 @@
+var currSlideIndex = 1;
+showSlides(currSlideIndex);
+
+function moveSlides(index) {
+    showSlides(currSlideIndex += index);
+}
+
+function currentSlide(index) {
+    showSlides(currSlideIndex = index);
+}
+
+function showSlides(index) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+   console.log(slides.length)
+
+    if (index > slides.length) {
+        currSlideIndex = 1;
+    }
+    if (index < 1) {
+        currSlideIndex = slides.length
+    }
+    //make all slides invisible
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    //display correct slide
+    slides[currSlideIndex - 1].style.display = "block";
+}
